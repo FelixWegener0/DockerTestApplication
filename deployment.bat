fada3ba0fc3487e594605f3558c7fc1400e348f3
Felix Wegener <Felix.Wegener05@t-online.de>
2024-09-18 09:35:17 +0200

    fix conatiner is allready running in deployment.bat

--- deployment.bat
+++ deployment.bat
@@ -3,7 +3,6 @@
 @REM set enviormental variables
 set dockerHubName=felixwegener
 set containerName=nodejs-image-backend
-set backendPort=4000
 
 @REM build and run docker container
 echo deploy nodejs backend application to docker container
@@ -16,10 +15,15 @@ echo build container
 docker build -t %dockerHubName%/%containerName% .
 echo:
 
-echo start container and set ports
-docker run --name %containerName% -p %backendPort%:%backendPort% -d %dockerHubName%/%containerName%
+cd ..
+
+echo pull docker container in compose
+cd docker
+docker compose up -d
+cd ..
 echo:
 
-cd ..
+echo copy frontend working dir to docker container webside dir
+echo f | xcopy /f /y "./frontend" "./docker/website"
 
 pause
